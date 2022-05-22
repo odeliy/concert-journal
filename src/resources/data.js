0200fdf1aefc2f14db9123200cdd6f08452ce999
cody iott <codyleeiott@gmail.com>
2022-05-22 12:33:04 -0700

    assign to variable before export

--- src/resources/data.js
+++ src/resources/data.js
@@ -1,47 +1,57 @@
-export default [
-    {
-        id:5,
-        title: "The War on Drugs",
-        location: "Keller Auditorium",
-        googleMapsUrl: "https://goo.gl/maps/1rhYEFxjfNEvwkH78",
-        date: "12 Jan, 2019",
-        description: "The War on Drugs is an American rock band from Philadelphia, Pennsylvania, formed in 2005. The band consists of Adam Granduciel (vocals, guitar), David Hartley (bass guitar), Robbie Bennett (keyboards), Charlie Hall (drums), Jon Natchez (saxophone, keyboards), Anthony LaMarca (guitar) and Eliza Hardy Jones (keyboards).",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/The_War_on_Drugs_in_Berkeley_CA_on_10-6-17.jpg/800px-The_War_on_Drugs_in_Berkeley_CA_on_10-6-17.jpg"
-    },
-    {
-        id:4,
-        title: "Hand Habits",
-        location: "Aladdin Theater",
-        googleMapsUrl: "https://goo.gl/maps/7azjEeUYeFQZMU918",
-        date: "28 Feb, 2018",
-        description: "Meg Duffy is an American musician and guitarist who has played as a studio musician on records by The War on Drugs, Weyes Blood, and William Tyler, among others. Duffy was also a member of Kevin Morby's live band, and is the sole member of indie-rock band Hand Habits. Duffy's Hand Habits project has been compared to Angel Olsen and Big Thief.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Meg_Duffy_performing_with_Hand_Habits_at_the_Brudenell_Socia_Club%2C_Leeds%2C_UK%2C_2019.jpg/800px-Meg_Duffy_performing_with_Hand_Habits_at_the_Brudenell_Socia_Club%2C_Leeds%2C_UK%2C_2019.jpg"
-    },
-    {
-        id:3,
-        title: "Grimes",
-        location: "Saturn",
-        googleMapsUrl: "https://goo.gl/maps/nJggkFt1iaQdok2X9",
-        date: "13 Aug, 2014",
-        description: "Claire Elise Boucher, known professionally as Grimes, is a Canadian musician, singer, songwriter, and record producer.[3][4] Her music has incorporated elements of varied styles, including dream pop, electronic, R&B, and hip hop, while touching on science fiction and feminist themes.[5] She has released five studio albums.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Grimes_Governors_Ball_2014_03_%28cropped%29.png/800px-Grimes_Governors_Ball_2014_03_%28cropped%29.png"
-    },
-    {
-        id:2,
-        title: "Screaming Females",
-        location: "Purgatory At The Masquerade",
-        googleMapsUrl: "https://goo.gl/maps/wqhaF8VWG3yZippa9",
-        date: "18 Dec, 2013",
-        description: "Screaming Females is an American rock band from New Brunswick, New Jersey, United States. They have released seven albums. The band comprises Marissa Paternoster on guitar and vocals, Jarrett Dougherty on drums, and Mike King Mike Abbate on bass.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Screaming_Females_in_Oslo_2012.jpg/800px-Screaming_Females_in_Oslo_2012.jpg"
-    },
-    {
-        id:1,
-        title: "Bright Eyes",
-        location: "Sloss Furnace",
-        googleMapsUrl: "https://goo.gl/maps/PNiPsYQtkLee1pUM9",
-        date: "4 Jun, 2013",
-        description: "Bright Eyes is an American indie rock band founded by singer-songwriter and guitarist Conor Oberst. It consists of Oberst, multi-instrumentalist and producer Mike Mogis, arranger, composer and trumpet and piano player Nate Walcott, and a rotating line-up of collaborators drawn primarily from Omaha's indie music scene.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/BrightEyes-2007.jpg/800px-BrightEyes-2007.jpg"
-    }
-]
+export default data = [
+  {
+    id: 5,
+    title: "The War on Drugs",
+    location: "Keller Auditorium",
+    googleMapsUrl: "https://goo.gl/maps/1rhYEFxjfNEvwkH78",
+    date: "12 Jan, 2019",
+    description:
+      "The War on Drugs is an American rock band from Philadelphia, Pennsylvania, formed in 2005. The band consists of Adam Granduciel (vocals, guitar), David Hartley (bass guitar), Robbie Bennett (keyboards), Charlie Hall (drums), Jon Natchez (saxophone, keyboards), Anthony LaMarca (guitar) and Eliza Hardy Jones (keyboards).",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/The_War_on_Drugs_in_Berkeley_CA_on_10-6-17.jpg/800px-The_War_on_Drugs_in_Berkeley_CA_on_10-6-17.jpg",
+  },
+  {
+    id: 4,
+    title: "Hand Habits",
+    location: "Aladdin Theater",
+    googleMapsUrl: "https://goo.gl/maps/7azjEeUYeFQZMU918",
+    date: "28 Feb, 2018",
+    description:
+      "Meg Duffy is an American musician and guitarist who has played as a studio musician on records by The War on Drugs, Weyes Blood, and William Tyler, among others. Duffy was also a member of Kevin Morby's live band, and is the sole member of indie-rock band Hand Habits. Duffy's Hand Habits project has been compared to Angel Olsen and Big Thief.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Meg_Duffy_performing_with_Hand_Habits_at_the_Brudenell_Socia_Club%2C_Leeds%2C_UK%2C_2019.jpg/800px-Meg_Duffy_performing_with_Hand_Habits_at_the_Brudenell_Socia_Club%2C_Leeds%2C_UK%2C_2019.jpg",
+  },
+  {
+    id: 3,
+    title: "Grimes",
+    location: "Saturn",
+    googleMapsUrl: "https://goo.gl/maps/nJggkFt1iaQdok2X9",
+    date: "13 Aug, 2014",
+    description:
+      "Claire Elise Boucher, known professionally as Grimes, is a Canadian musician, singer, songwriter, and record producer.[3][4] Her music has incorporated elements of varied styles, including dream pop, electronic, R&B, and hip hop, while touching on science fiction and feminist themes.[5] She has released five studio albums.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Grimes_Governors_Ball_2014_03_%28cropped%29.png/800px-Grimes_Governors_Ball_2014_03_%28cropped%29.png",
+  },
+  {
+    id: 2,
+    title: "Screaming Females",
+    location: "Purgatory At The Masquerade",
+    googleMapsUrl: "https://goo.gl/maps/wqhaF8VWG3yZippa9",
+    date: "18 Dec, 2013",
+    description:
+      "Screaming Females is an American rock band from New Brunswick, New Jersey, United States. They have released seven albums. The band comprises Marissa Paternoster on guitar and vocals, Jarrett Dougherty on drums, and Mike King Mike Abbate on bass.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Screaming_Females_in_Oslo_2012.jpg/800px-Screaming_Females_in_Oslo_2012.jpg",
+  },
+  {
+    id: 1,
+    title: "Bright Eyes",
+    location: "Sloss Furnace",
+    googleMapsUrl: "https://goo.gl/maps/PNiPsYQtkLee1pUM9",
+    date: "4 Jun, 2013",
+    description:
+      "Bright Eyes is an American indie rock band founded by singer-songwriter and guitarist Conor Oberst. It consists of Oberst, multi-instrumentalist and producer Mike Mogis, arranger, composer and trumpet and piano player Nate Walcott, and a rotating line-up of collaborators drawn primarily from Omaha's indie music scene.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/BrightEyes-2007.jpg/800px-BrightEyes-2007.jpg",
+  },
+];
